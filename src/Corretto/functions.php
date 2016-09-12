0c59f4f8a0a7c16f58afb0ca62e7962e80790f99
Payton Swick <payton@foolord.com>
2016-09-11 23:22:15 -0400

    Add aliases for BDD and TDD

--- src/Corretto/functions.php
+++ src/Corretto/functions.php
@@ -12,12 +12,29 @@ function describe( string $name, callable $callable ) {
 	$runner->addSuite( $desc );
 }
 
+function context( string $name, callable $callable = null ) {
+	describe( $name, $callable );
+}
+
+function suite( string $name, callable $callable = null ) {
+	describe( $name, $callable );
+}
+
+function specify( string $name, callable $callable = null ) {
+	it( $name, $callable );
+}
+
+function test( string $name, callable $callable = null ) {
+	it( $name, $callable );
+}
+
 function it( string $name, callable $callable = null ) {
 	global $runner;
 	$test = new Test( $name, $callable );
 	$runner->addTest( $test );
 }
 
+// TODO: find a way to skip a suite
 function skip( string $name, callable $callable = null ) {
 	global $runner;
 	$test = new Test( $name, $callable );
@@ -25,6 +42,7 @@ function skip( string $name, callable $callable = null ) {
 	$runner->addTest( $test );
 }
 
+// TODO: move to assertions file
 function assert( $expression = false ) {
 	if ( ! $expression ) {
 		throw new \Exception( "Failed asserting that '" . var_export( $expression, true ) . "' is true" );
