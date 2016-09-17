5631c01af41fa7a5adacc0b9cf10745ddf0046e4
Payton Swick <payton@foolord.com>
2016-09-17 00:35:56 -0700

    Add initial `expect` behavior

---
+++ src/Corretto/Expect.php
@@ -0,0 +1,26 @@
+<?php
+namespace Corretto;
+
+function expect( $actual ) {
+	return new Expectation( $actual );
+}
+
+class Expectation {
+	function __construct( $actual ) {
+		$this->actual = $actual;
+	}
+
+	public function toBeTrue() {
+		return assert( $this->actual );
+	}
+
+	public function toBeFalse() {
+		return assertFalse( $this->actual );
+	}
+
+	public function toEqual( $expected ) {
+	}
+
+	public function toNotEqual( $expected ) {
+	}
+}
---
+++ tests/Expect.php
@@ -0,0 +1,79 @@
+<?php
+
+use function \Corretto\{describe, it, expect};
+
+describe( 'expect()', function() {
+	describe( 'toBeTrue()', function() {
+		it( 'passes if the actual is true', function() {
+			expect( true )->toBeTrue();
+		} );
+
+		it( 'fails if its argument is false', function() {
+			try {
+				expect( false )->toBeTrue();
+			} catch ( Exception $e ) {
+				return;
+			}
+			throw new Exception( 'toBeTrue() did not fail as expected' );
+		} );
+
+		it( 'fails with an expected string if its argument is false', function() {
+			try {
+				expect( false )->toBeTrue();
+			} catch ( Exception $e ) {
+				$expected = "Failed asserting that 'false' is true";
+				if ( $e->getMessage() === $expected ) {
+					return;
+				}
+				throw new Exception( 'toBeTrue() did not have the expected string. Instead it said: ' . $e->getMessage() );
+			}
+			throw new Exception( 'toBeTrue() did not fail as expected' );
+		} );
+	} );
+
+	describe( 'toBeFalse()', function() {
+		it( 'passes if the actual is false', function() {
+			expect( false )->toBeFalse();
+		} );
+
+		it( 'fails if its argument is true', function() {
+			try {
+				expect( true )->toBeFalse();
+			} catch ( Exception $e ) {
+				return;
+			}
+			throw new Exception( 'toBeFalse() did not fail as expected' );
+		} );
+
+		it( 'fails with an expected string if its argument is true', function() {
+			try {
+				expect( true )->toBeFalse();
+			} catch ( Exception $e ) {
+				$expected = "Failed asserting that 'true' is false";
+				if ( $e->getMessage() === $expected ) {
+					return;
+				}
+				throw new Exception( 'toBeFalse() did not have the expected string. Instead it said: ' . $e->getMessage() );
+			}
+			throw new Exception( 'toBeFalse() did not fail as expected' );
+		} );
+	} );
+
+	describe( 'toEqual()', function() {
+		it( 'passes if its argument equals the actual', function() {
+			expect( 'hello' )->toEqual( 'hello' );
+		} );
+
+		it( 'fails if its argument does not equal the actual' );
+		it( 'fails with an expected string if the argument does not equal the actual' );
+	} );
+
+	describe( 'toNotEqual()', function() {
+		it( 'passes if its argument does not equal the actual', function() {
+			expect( 'bye' )->toNotEqual( 'hello' );
+		} );
+
+		it( 'fails if its argument equals the actual' );
+		it( 'fails with an expected string if the argument equals the actual' );
+	} );
+} );
