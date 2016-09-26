13cabe077d0e3ecc749bfa2db1af8b55453d1efe
Payton Swick <payton@foolord.com>
2016-09-26 18:52:59 -0400

    Add before/after tests to examples

--- examples/describe.php
+++ examples/describe.php
@@ -4,6 +4,7 @@ use function \Corretto\{describe, it};
 use function \Corretto\{test, suite};
 use function \Corretto\{context, specify};
 use function \Corretto\{assertTrue, assertFalse, assertEquals, assertNotEquals};
+use function \Corretto\{beforeEach, afterEach, before, after};
 use function \Corretto\{expect};
 use const \Corretto\SKIP;
 
@@ -100,6 +101,70 @@ suite( 'my tests', function() {
 			test( '->toNotEqual()', function() {
 				expect( 'hi' )->toNotEqual( 'bye' );
 			} );
+		} );
+	} );
+} );
+
+describe( 'set up and tear down', function() {
+	describe( 'beforeEach()', function() {
+		beforeEach( function( $ctx ) {
+			$ctx->name = 'hello';
+		} );
+
+		it( 'sets up the test context', function( $ctx ) {
+			expect( $ctx->name )->toEqual( 'hello' );
+			$ctx->name = 'bye';
+		} );
+
+		it( 'runs again before each test', function( $ctx ) {
+			expect( $ctx->name )->toNotEqual( 'bye' );
+		} );
+	} );
+
+	describe( 'before()', function() {
+		before( function( $ctx ) {
+			$ctx->name = 'hello';
+		} );
+
+		it( 'sets up the test context', function( $ctx ) {
+			expect( $ctx->name )->toEqual( 'hello' );
+			$ctx->name = 'bye';
+		} );
+
+		it( 'runs only once before the suite runs', function( $ctx ) {
+			expect( $ctx->name )->toEqual( 'bye' );
+		} );
+	} );
+
+	describe( 'afterEach()', function() {
+		$name = 'hello';
+		afterEach( function() use ( &$name ) {
+			$name = 'bye';
+		} );
+
+		it( 'is run after each test', function() use( &$name ) {
+			expect( $name )->toEqual( 'hello' );
+		} );
+
+		it( 'runs again after each test', function() use ( &$name ) {
+			expect( $name )->toEqual( 'bye' );
+		} );
+	} );
+
+	$name = 'hello';
+	describe( 'after()', function() use( &$name ) {
+		after( function() use ( &$name ) {
+			$name = 'bye';
+		} );
+
+		it( 'is run after all tests in a suite', function() use( &$name ) {
+			expect( $name )->toEqual( 'hello' );
+		} );
+	} );
+
+	describe( 'after() (continued)', function() use( &$name ) {
+		it( 'is run at the end of a suite', function() use ( &$name ) {
+			expect( $name )->toEqual( 'bye' );
 		} );
 	} );
 } );
