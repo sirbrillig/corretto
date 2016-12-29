cbf0984369903f8d7f8a7d6db714ac1b8e8c337c
Payton Swick <payton@foolord.com>
2016-12-29 14:03:15 -0500

    Add extendExpectation() function

--- src/Corretto/Expect.php
+++ src/Corretto/Expect.php
@@ -5,9 +5,29 @@ function expect( $actual ) {
 	return new Expectation( $actual );
 }
 
+function extendExpectation( $newExpectation ) {
+	Expectation::extendExpectation( $newExpectation );
+}
+
 class Expectation {
+	private static $extensions = [];
+
+	public static function extendExpectation( $Extension ) {
+		self::$extensions[] = $Extension;
+	}
+
 	function __construct( $actual ) {
 		$this->actual = $actual;
+	}
+
+	public function __call( $name, $arguments ) {
+		foreach( self::$extensions as $Extension ) {
+			$instance = new $Extension( $this->actual );
+			if ( method_exists( $instance, $name ) ) {
+				return call_user_func_array( [ $instance, $name ], $arguments );
+			}
+		}
+		throw new \Exception( "Call to undefined method Corretto\Expectation::$name()" );
 	}
 
 	public function toBeTrue() {
@@ -22,24 +42,6 @@ class Expectation {
 		if ( $expression ) {
 			throw new AssertionFailure( "Failed asserting that '" . var_export( $expression, true ) . "' is false" );
 		}
-	}
-
-	private function recursiveAssocSort( $elements ) {
-		if ( ! is_array( $elements ) ) {
-			return $elements;
-		}
-		$elements = array_map( function( $el ) {
-			return $this->recursiveAssocSort( $el );
-		}, $elements );
-		if ( ! $this->hasStringKeys( $elements ) ) {
-			return $elements;
-		}
-		sort( $elements );
-		return $elements;
-	}
-
-	private function hasStringKeys( array $array ) {
-		return count( array_filter( array_keys( $array ), 'is_string' ) ) > 0;
 	}
 
 	public function toEqual( $expected ) {
@@ -93,6 +95,30 @@ class Expectation {
 		}
 	}
 
+	private function recursiveAssocSort( $elements ) {
+		if ( ! is_array( $elements ) ) {
+			return $elements;
+		}
+		$elements = array_map( function( $el ) {
+			return $this->recursiveAssocSort( $el );
+		}, $elements );
+		if ( ! $this->hasStringKeys( $elements ) ) {
+			return $elements;
+		}
+		sort( $elements );
+		return $elements;
+	}
+
+	private function hasStringKeys( array $array ) {
+		return count( array_filter( array_keys( $array ), 'is_string' ) ) > 0;
+	}
+}
+
+class ContainExpectation {
+	function __construct( $actual ) {
+		$this->actual = $actual;
+	}
+
 	public function toContain( $expected ) {
 		$actual = $this->actual;
 		if ( is_string( $actual ) && strpos( $actual, $expected ) === false ) {
@@ -121,3 +147,6 @@ class Expectation {
 		}
 	}
 }
+
+extendExpectation( '\Corretto\ContainExpectation' );
+
